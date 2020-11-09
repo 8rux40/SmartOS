9e88eca4c21a4df24b8cce4cc62c7bbe3a05fba5
Bruno Tardin <brunotardin20@gmail.com>
2020-11-09 14:35:53 -0300

    Configurando dependências

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,22 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.scripts([
+    'node_modules/moment/min/moment-with-locales.min.js',
+    /*'node_modules/jquery/dist/jquery.js',
+    'node_modules/jquery-mask-plugin/dist/jquery.mask.js',
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+    'node_modules/sweetalert2/dist/sweetalert2.all.js',
+    'node_modules/pdfmake/build/pdfmake.js',
+    'node_modules/datatables.net/js/jquery.dataTables.js',
+    'node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js',
+    'node_modules/datatables.net-buttons/js/dataTables.buttons.js',
+    'node_modules/datatables.net-buttons-bs4/js/buttons.bootstrap4.js',
+    'node_modules/datatables.net-colreorder/js/dataTables.colReorder.js',
+    'node_modules/datatables.net-colreorder-bs4/js/colReorder.bootstrap4.js',
+    'node_modules/datatables.net-select/js/dataTables.select.js',
+    'node_modules/datatables.net-select-bs4/js/select.bootstrap4.js',
+    */
+], 'public/js/app.js').
+sass('resources/sass/app.scss', 'public/css/app.css').
+sourceMaps(true, "source-maps")
