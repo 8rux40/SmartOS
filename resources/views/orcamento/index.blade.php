a8f01d512b0b230defabcffee1dcb72b2ba75d5d
Bruno Tardin <brunotardin20@gmail.com>
2020-11-14 20:23:36 -0300

    Gerenciar orçamentos

--- resources/views/orcamento/index.blade.php
+++ resources/views/orcamento/index.blade.php
@@ -2,66 +2,111 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-      <div class="col-md-9">
-        <h3><i class="fas fa-coins text-success"></i> Orçamentos</li> </h3> 
-      </div>      
-    </div>
+  <div class="row">
+    <div class="col-md-9">
+      <h3><i class="fas fa-coins text-success"></i> Orçamentos pendentes</li> </h3> 
+    </div>      
+  </div>
   <div class="mt-2"></div>
     <div class="row">
       <div class="col-md-12">
         <div class="responsive-table">
-          <table class="table">
+          <table class="table table-striped" id="orcamento-pendente">
             <thead class="thead-dark">
               <tr>
-                <th scope="col">#</th>
-                <th scope="col">Descrição</th>
-                <th scope="col">Valor</th>
-                <th scope="col">Ações</th>
+                <th scope="col">Data</th>
+                <th scope="col">Cliente</th>
+                <th scope="col">Celular</th>
+                <th scope="col"></th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>
-                  <button type="button" class="btn btn-outline-success">
-                    <i class="fas fa-check"></i>
-                  </button>
-                  <button type="button" class="btn btn-outline-danger">
-                    <i class="fas fa-trash"></i>
-                  </button>
-                </td>
-              </tr>
             </tbody>
           </table>
         </div>
       </div>          
-    </div>        
+    </div>  
+    <br><br>
+    <div class="row">
+      <div class="col-md-9">
+        <h3><i class="fas fa-coins text-success"></i> Orçamentos informados</li> </h3> 
+      </div>      
+    </div>
+    <div class="mt-2"></div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="responsive-table">
+            <table class="table table-striped" id="orcamento-informado">
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">Data</th>
+                  <th scope="col">Cliente</th>
+                  <th scope="col">Celular</th>
+                  <th scope="col">Valor (R$)</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+          </div>
+        </div>          
+      </div>            
   </div>
 @endsection
 
 @push('javascript')
 <script>
-  const config = {
-    method: 'get'
+  $(document).ready(function(){
+    carregaValores()
+  })
+  
+  function carregaValores(){
+    const url = "{{ route('orcamento.getAll') }}";
+    $.getJSON(url, function (data){
+      if (Array.isArray(data) && data.length){
+        data.forEach(orcamento => {
+          if (orcamento.status == 1){
+            row = '<tr>';
+            row += '<td>'+ moment(orcamento.created_at).format('DD/MM/yyyy') +'</td>';
+            row += '<td>'+ orcamento.cliente.nome +'</td>';
+            row += '<td>'+ orcamento.celular.marca + ' ' + orcamento.celular.modelo +'</td>';
+            row += `<td class="text-center">
+                        <a href="{{route('orcamento.show',':id')}}" class="btn btn-sm btn-primary" title="Ver Detalhes"><li class="fa fa-eye"></li></a>
+                        <a href="{{route('orcamento.edit',':id')}}" class="btn btn-sm btn-secondary" title="Editar"><li class="fa fa-edit"></li></a>
+                        <a href="excluirOrcamento(:id)" class="btn btn-sm btn-danger" title="Excluir"><li class="fa fa-trash"></li></a>
+                    </td>`.replaceAll(':id',orcamento.id,)
+            row += '</tr>';
+            $('table#orcamento-pendente tbody').append(row);
+          } else {
+            row = '<tr>';
+            row += '<td>'+ moment(orcamento.created_at).format('DD/MM/yyyy') +'</td>';
+            row += '<td>'+ orcamento.cliente.nome +'</td>';
+            row += '<td>'+ orcamento.celular.marca + ' ' + orcamento.celular.modelo +'</td>';
+            row += '<td>'+ orcamento.valor_orcamento +'</td>';
+            row += `<td class="text-center">
+                        <a href="{{route('orcamento.show',':id')}}" class="btn btn-sm btn-primary" title="Ver Detalhes"><li class="fa fa-eye"></li></a>
+                        <a href="{{route('ordemservico.create',':id')}}" class="btn btn-sm btn-primary" title="Ver Detalhes"><li class="fa fa-eye"></li></a>
+                        <a href="{{route('orcamento.edit',':id')}}" class="btn btn-sm btn-secondary" title="Editar"><li class="fa fa-edit"></li></a>
+                        <a href="excluirOrcamento(:id)" class="btn btn-sm btn-danger" title="Excluir"><li class="fa fa-trash"></li></a>
+                    </td>`.replaceAll(':id',orcamento.id,)
+            row += '</tr>';
+            $('table#orcamento-informado tbody').append(row);
+          }
+
+        });
+      } else {
+        const row = 
+          `
+            <tr>
+              <td colspan="13" class="text-center mx-auto">
+                  <p style="padding-top:0.8rem;">Não existem orcamentos cadastrados.</p>
+              </td>
+            </tr>
+          `;
+        $('table#orcamentos tbody').append(row);
+      }
+    })
   }
-
-  const url = "{{ route('orcamento.getAll') }}";
-
-  fetch(url, config)
-    .then(function(response){
-      console.log(response)
-    })
-
-    .catch(function(err) {
-      console.log(err)
-    })
-
-    // jQuery
-    $.getJSON(url, function(data){ console.log(data) })
-
-    
 </script>
 @endpush
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,25 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::prefix('ordemservico')->group(function(){
+    Route::get('/', [OrdemServicoController::class, 'index'])->name('ordemservico.index');
+    Route::get('/create/{orcamento_id}', [OrcamentoController::class, 'create'])->name('ordemservico.create');
+    Route::get('/show/{id}', [OrdemServicoController::class, 'show'])->name('ordemservico.show');
+    Route::get('/edit/{id}', [OrdemServicoController::class, 'edit'])->name('ordemservico.edit');
+    Route::get('/getAll', [OrdemServicoController::class, 'getAll'])->name('ordemservico.getAll');
+    
+    Route::put('/update', [OrdemServicoController::class, 'update'])->name('ordemservico.update');
+});
+
 Route::prefix('orcamento')->group(function(){
     Route::get('/', [OrcamentoController::class, 'index'])->name('orcamento.index');
     Route::get('/create/{cliente_id}/{celular_id}', [OrcamentoController::class, 'create'])->name('orcamento.create');
     Route::get('/show/{id}', [OrcamentoController::class, 'show'])->name('orcamento.show');
+    Route::get('/edit/{id}', [OrcamentoController::class, 'edit'])->name('orcamento.edit');
     Route::get('/getAll', [OrcamentoController::class, 'getAll'])->name('orcamento.getAll');
     
     Route::post('/solicitar', [OrcamentoController::class, 'solicitarOrcamento'])->name('orcamento.solicitar');
+    Route::put('/update', [OrcamentoController::class, 'update'])->name('orcamento.update');
 });
 
 Route::prefix('peca')->group(function(){
