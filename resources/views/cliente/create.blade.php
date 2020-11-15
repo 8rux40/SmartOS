15a028cec1ff98d4970fb7eb9dfc1d7b71351237
JOAO PAULO ALVES <joaopauloiff2018@gmail.com>
2020-11-15 15:11:14 -0300

    Criação da tela de cadastro de clientes

--- resources/views/cliente/create.blade.php
+++ resources/views/cliente/create.blade.php
@@ -1,13 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container">
   <form action="{{ route('cliente.create') }}" method="post" id="create">
-  @csrf            
+  @csrf   
+    <div class="row">
+        <div class="col-md-5">
+            <div class="form-group">
+                <label for="NomeCliente">Nome</label>
+                <input disabled="disabled" type="text" class="form-control" id="NomeCliente" required="true" name="cliente_nome">                
+            </div>
+        </div>
+        <div class="col-md-2">
+          <div class="form-group">
+              <label for="CpfCliente">CPF</label>
+              <input disabled="disabled" type="text" class="form-control" id="CpfCliente" required="true" name="cpf">
+          </div>
+      </div> 
+      <div class="col-md-2">
+          <div class="form-group">
+              <label for="TelefoneCliente">Número de telefone</label>
+              <input disabled="disabled" type="text" class="form-control" id="TelefoneCliente" name="cliente_numero_tel">
+          </div>
+      </div>
+      <div class="col-md-2">
+          <div class="form-group">
+              <label for="CelularCliente">Número de celular</label>
+              <input disabled="disabled" type="text" class="form-control" id="CelularCliente" name="cliente_numero_cel">
+          </div>
+      </div>                       
+    </div>
+    <div class="row"> 
+        <div class="col-md-3">
+            <div class="form-group">
+                <label for="EmailCliente">Email</label>
+                <input disabled="disabled" type="email" class="form-control" id="EmailCliente" aria-describedby="emailHelp" name="cliente_email">
+                <small id="emailHelp" class="form-text text-muted">Nós nunca iremos compartilhar o seu e-mail.</small>         
+          </div>
+        </div>      
+        <div class="col-md-6">
+          <div class="form-group">
+              <label for="EnderecoCliente">Endereço</label>
+              <input disabled="disabled" type="text" class="form-control" id="EnderecoCliente" required="true" name="cliente_endereco">
+          </div>
+        </div>                   
+    </div>
+    <div class="row">
+      <div class="col-md-2">
+          <button type="submit" class="btn btn-primary">Cadastrar cliente</button>
+      </div>            
+    </div>
+    </div>
   </form>
 </div>
-
 @endsection
 
 @push('javascript')
