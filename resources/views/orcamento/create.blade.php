baadbd98eda3c8ed51bab0579c9fd1502b092330
Bruno Tardin <brunotardin20@gmail.com>
2020-11-17 16:38:07 -0300

    Restruturação da página orcamento.create

--- resources/views/orcamento/create.blade.php
+++ resources/views/orcamento/create.blade.php
@@ -2,23 +2,59 @@
 
 @section('content')
 <div class="container">
+    <div class="row">
+        <div class="col-md-9">
+            <h3><i class="fas fa-coins text-primary"></i> Novo orçamento</li> </h3>         
+        </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
   <form action="{{ route('orcamento.solicitar') }}" method="post" id="solicitarOrcamento">
   @csrf
   <input type="hidden" name="cliente_id" value="{{ $cliente->id }}">
   <input type="hidden" name="celular_id" value="{{ $celular->id }}">
   <div class="row">
-      <div class="col-md">
-          <div class="form-group">
-              <label for="InputImei1">Primeiro Imei do celular</label>
-              <input disabled="disabled" type="text" class="form-control" id="InputImei1" name="celular_imei" value="{{ $celular->imei }}">
-          </div>
-      </div> 
-      <div class="col-md">
-          <div class="form-group">
-              <label for="InputImei2">Segundo Imei do celular</label>
-              <input disabled="disabled" type="text" class="form-control" id="InputImei2" name="celular_imei2" value="{{ $celular->imei2 }}">
-          </div>
-      </div> 
+    <div class="col-md-6">
+        <div class="form-group">
+            <label for="NomeCliente">Nome do cliente</label>
+            <input disabled="disabled" type="text" class="form-control" id="NomeCliente" required="true" name="cliente_nome" value="{{ $cliente->nome }}">
+        </div>
+    </div>
+    <div class="col-md-2">
+        <div class="form-group">
+            <label for="CpfCliente">CPF</label>
+            <input disabled="disabled" type="text" class="form-control" id="CpfCliente" required="true" name="cpf" value="{{ $cliente->cpf }}">
+        </div>
+    </div> 
+    <div class="col-md-2">
+        <div class="form-group">
+            <label for="TelefoneCliente">Número de telefone</label>
+            <input disabled="disabled" type="text" class="form-control" id="TelefoneCliente" name="cliente_numero_tel" value="{{ $cliente->numero_tel }}">
+        </div>
+    </div>
+    <div class="col-md-2">
+        <div class="form-group">
+            <label for="CelularCliente">Número de celular</label>
+            <input disabled="disabled" type="text" class="form-control" id="CelularCliente" name="cliente_numero_cel" value="{{ $cliente->numero_cel }}">
+        </div>
+    </div>                       
+</div>
+<div class="row">
+    <div class="col-md-8">
+        <div class="form-group">
+            <label for="EnderecoCliente">Endereço</label>
+            <input disabled="disabled" type="text" class="form-control" id="EnderecoCliente" required="true" name="cliente_endereco" value="{{ $cliente->endereco }}">
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="form-group">
+            <label for="EmailCliente">Email</label>
+            <input disabled="disabled" type="email" class="form-control" id="EmailCliente" aria-describedby="emailHelp" name="cliente_email" value="{{ $cliente->email }}">         
+        </div>
+    </div>
+</div>
+<hr>
+  <div class="row">
       <div class="col">
           <div class="form-group">
               <label for="MarcaCelular">Marca do celular</label>
@@ -31,51 +67,22 @@
               <input disabled="disabled" type="text" class="form-control" id="ModeloCelular" required="true"  name="celular_modelo" value="{{ $celular->modelo }}">
           </div>
       </div>          
-  </div>
-
-  <div class="row">
-      <div class="col-md-5">
+      <div class="col-md">
           <div class="form-group">
-              <label for="NomeCliente">Nome do cliente</label>
-              <input disabled="disabled" type="text" class="form-control" id="NomeCliente" required="true" name="cliente_nome" value="{{ $cliente->nome }}">
-          </div>
-      </div>
-      <div class="col-md-2">
-          <div class="form-group">
-              <label for="CpfCliente">CPF</label>
-              <input disabled="disabled" type="text" class="form-control" id="CpfCliente" required="true" name="cpf" value="{{ $cliente->cpf }}">
+              <label for="InputImei1">Primeiro Imei do celular</label>
+              <input disabled="disabled" type="text" class="form-control" id="InputImei1" name="celular_imei" value="{{ $celular->imei }}">
           </div>
       </div> 
-      <div class="col-md-2">
+      <div class="col-md">
           <div class="form-group">
-              <label for="TelefoneCliente">Número de telefone</label>
-              <input disabled="disabled" type="text" class="form-control" id="TelefoneCliente" name="cliente_numero_tel" value="{{ $cliente->numero_tel }}">
+              <label for="InputImei2">Segundo Imei do celular</label>
+              <input disabled="disabled" type="text" class="form-control" id="InputImei2" name="celular_imei2" value="{{ $celular->imei2 }}">
           </div>
-      </div>
-      <div class="col-md-2">
-          <div class="form-group">
-              <label for="CelularCliente">Número de celular</label>
-              <input disabled="disabled" type="text" class="form-control" id="CelularCliente" name="cliente_numero_cel" value="{{ $cliente->numero_cel }}">
-          </div>
-      </div>                       
-  </div>
+      </div> 
+    </div>
+  <hr>
   <div class="row">
-      <div class="col-md-7">
-          <div class="form-group">
-              <label for="EnderecoCliente">Endereço</label>
-              <input disabled="disabled" type="text" class="form-control" id="EnderecoCliente" required="true" name="cliente_endereco" value="{{ $cliente->endereco }}">
-          </div>
-      </div>
-      <div class="col-md-5">
-          <div class="form-group">
-              <label for="EmailCliente">Email</label>
-              <input disabled="disabled" type="email" class="form-control" id="EmailCliente" aria-describedby="emailHelp" name="cliente_email" value="{{ $cliente->email }}">
-              <small id="emailHelp" class="form-text text-muted">Nós nunca iremos compartilhar o seu e-mail.</small>         
-          </div>
-      </div>
-  </div>
-  <div class="row">
-      <div class="col-md-7">
+      <div class="col-md-12">
           <div class="form-group">
               <label for="DescricaoProblema">Descrição do problema do celular</label>
               <textarea class="form-control" id="DescricaoProblema" rows="4" required name="descricao_problema"></textarea>
@@ -83,11 +90,13 @@
       </div>
   </div> 
   <div class="row">
-      <div class="col-md-2">
-          <button type="submit" class="btn btn-primary">Solicitar orçamento</button>
+      <div class="col-md-12">
+          <button type="submit" class="btn btn-success float-right"><i class="fas fa-check"></i>&nbsp;Solicitar orçamento</button>
       </div>            
   </div>             
   </form>
+</div>
+</div>
 @endsection
 
 @push('javascript')
@@ -115,12 +124,12 @@
                 confirmButtonColor: '#3085d6',
                 cancelButtonColor: '#888',
                 confirmButtonText: 'Ver orçamento cadastrado',
-                cancelButtonText: 'Cadastrar outro orçamento'
+                cancelButtonText: 'Voltar ao cliente'
             }).then((result) => {
                 if (result.value) {
                     $(location).attr('href',response.route);
                 } else {
-                    //limparFormulario()
+                    $(location).attr('href', "{{ route('cliente.show', $cliente->id) }}")
                 }
             })
         } else {
