a5d774cf4a497545a22c469894c6436aab71c11e
SamirAssad-coder <samir_caverna@hotmail.com>
2020-11-16 22:56:19 -0300

    tornando pag info cliente dinamica

--- resources/views/celular/edit.blade.php
+++ resources/views/celular/edit.blade.php
@@ -2,63 +2,38 @@
 
 @section('content')
 <div class="container">
-<<<<<<< HEAD
   <form method="post" id="formEditCelular">
   @csrf   
   @method('PUT')
-=======
-  <form action="{{ route('celular.edit') }}" method="post" id="formCreateCelular">
-  @csrf   
->>>>>>> 7251066b81880d4a2b5ec48e4b82410d877bbfa6
     <div class="row">
       <div class="col-md">
         <div class="form-group">
           <label for="InputImei1">Primeiro Imei do celular</label>
-<<<<<<< HEAD
           <input  type="text" class="form-control" id="InputImei1" name="celular_imei" value= "{{ $celular->imei }}">
-=======
-          <input  type="text" class="form-control" id="InputImei1" name="celular_imei">
->>>>>>> 7251066b81880d4a2b5ec48e4b82410d877bbfa6
         </div>
       </div> 
       <div class="col-md">
         <div class="form-group">
           <label for="InputImei2">Segundo Imei do celular</label>
-<<<<<<< HEAD
           <input  type="text" class="form-control" id="InputImei2" name="celular_imei2" value= "{{ $celular->imei2 }}">
-=======
-          <input  type="text" class="form-control" id="InputImei2" name="celular_imei2">
->>>>>>> 7251066b81880d4a2b5ec48e4b82410d877bbfa6
         </div>
       </div> 
       <div class="col">
         <div class="form-group">
           <label for="MarcaCelular">Marca do celular</label>
-<<<<<<< HEAD
           <input  type="text" class="form-control" id="MarcaCelular" required="true" name="celular_marca" value= "{{$celular->marca}}">
-=======
-          <input  type="text" class="form-control" id="MarcaCelular" required="true" name="celular_marca">
->>>>>>> 7251066b81880d4a2b5ec48e4b82410d877bbfa6
         </div>
       </div>
       <div class="col">
         <div class="form-group">
           <label for="ModeloCelular">Modelo do celular</label>
-<<<<<<< HEAD
           <input type="text" class="form-control" id="ModeloCelular" required="true"  name="celular_modelo" value= "{{$celular->modelo}}">
-=======
-          <input type="text" class="form-control" id="ModeloCelular" required="true"  name="celular_modelo">
->>>>>>> 7251066b81880d4a2b5ec48e4b82410d877bbfa6
         </div>
       </div>
     </div>
     <div class="row">
       <div class="col-md-2">
-<<<<<<< HEAD
           <button type="submit" class="btn btn-primary">Editar celular</button>
-=======
-          <button type="submit" class="btn btn-primary">Cadastrar celular</button>
->>>>>>> 7251066b81880d4a2b5ec48e4b82410d877bbfa6
       </div>            
     </div>
   </form>
@@ -67,7 +42,6 @@
 
 @push('javascript')
 <script>
-<<<<<<< HEAD
  $('#formEditCelular').submit(function(e){
   e.preventDefault();
    var serializedData = $(this).serialize();
@@ -102,8 +76,5 @@
     }
   });
  });
-=======
-
->>>>>>> 7251066b81880d4a2b5ec48e4b82410d877bbfa6
 </script>
 @endpush
