45a60ea6f999e1356d9554b93e939262ae8d4bf8
SamirAssad-coder <samir_caverna@hotmail.com>
2020-11-17 17:29:10 -0300

    start ordemservico.create

--- resources/views/orcamento/index.blade.php
+++ resources/views/orcamento/index.blade.php
@@ -86,7 +86,7 @@
             row += '<td>'+ orcamento.valor_orcamento +'</td>';
             row += `<td class="text-center">
                         <a href="{{route('orcamento.show',':id')}}" class="btn btn-sm btn-primary" title="Ver Detalhes"><li class="fa fa-eye"></li></a>
-                        <a href="{{route('ordemservico.create',':id')}}" class="btn btn-sm btn-success" title="Ver Detalhes"><li class="fa fa-plus"></li></a>
+                        <a href="{{route('ordemservico.create',':id')}}" class="btn btn-sm btn-success" title="Criar Ordem de Serviço"><li class="fa fa-plus"></li></a>
                         <a href="{{route('orcamento.edit',':id')}}" class="btn btn-sm btn-secondary" title="Editar"><li class="fa fa-edit"></li></a>
                         <a href="excluirOrcamento(:id)" class="btn btn-sm btn-danger" title="Excluir"><li class="fa fa-trash"></li></a>
                     </td>`.replaceAll(':id',orcamento.id,)
