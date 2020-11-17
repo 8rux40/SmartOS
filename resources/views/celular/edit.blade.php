787f7e553509ef7c02e6937a794e08f5899ae1d9
JOAO PAULO ALVES <joaopauloiff2018@gmail.com>
2020-11-16 22:20:15 -0300

    Tela de editar celular feito

---
+++ resources/views/celular/edit.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <form method="post" id="formEditCelular">
+  @csrf   
+  @method('PUT')
+    <div class="row">
+      <div class="col-md">
+        <div class="form-group">
+          <label for="InputImei1">Primeiro Imei do celular</label>
+          <input  type="text" class="form-control" id="InputImei1" name="celular_imei" value= "{{ $celular->imei }}">
+        </div>
+      </div> 
+      <div class="col-md">
+        <div class="form-group">
+          <label for="InputImei2">Segundo Imei do celular</label>
+          <input  type="text" class="form-control" id="InputImei2" name="celular_imei2" value= "{{ $celular->imei2 }}">
+        </div>
+      </div> 
+      <div class="col">
+        <div class="form-group">
+          <label for="MarcaCelular">Marca do celular</label>
+          <input  type="text" class="form-control" id="MarcaCelular" required="true" name="celular_marca" value= "{{$celular->marca}}">
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="ModeloCelular">Modelo do celular</label>
+          <input type="text" class="form-control" id="ModeloCelular" required="true"  name="celular_modelo" value= "{{$celular->modelo}}">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-2">
+          <button type="submit" class="btn btn-primary">Editar celular</button>
+      </div>            
+    </div>
+  </form>
+</div>
+@endsection
+
+@push('javascript')
+<script>
+ $('#formEditCelular').submit(function(e){
+  e.preventDefault();
+   var serializedData = $(this).serialize();
+
+  $.ajax({
+    url: "{{ route('celular.update', $celular->id) }}",
+    method: 'put',
+    dataType: 'json',
+    data: serializedData,
+    success: function (response) {
+      console.log(response)
+      if(response.success) {
+        Swal.fire({
+          title: 'Celular editado com sucesso!',
+                text: response.message,
+                icon: 'success',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#888',
+                confirmButtonText: 'Ver celular cadastrado',
+                cancelButtonText: 'Cadastrar outro celular'
+        }).then((result) => {
+          if(result.value) {
+            $(location).attr('href', response.route);
+          } else {
+            //limparFormulario()
+          }
+        })
+      } else {
+        //mostrarErros(response.errors);
+      }
+    }
+  });
+ });
+</script>
+@endpush
--- resources/views/cliente/edit.blade.php
+++ resources/views/cliente/edit.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
 
-
-
 @endsection
 
 @push('javascript')
