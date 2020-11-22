4afa0a0bc3e3ce21b8ce190ca07face0c500fbb6
JOAO PAULO ALVES <joaopauloiff2018@gmail.com>
2020-11-22 08:35:30 -0300

    Inclusão dos componentes de pesquisa de peca

--- resources/views/peca/index.blade.php
+++ resources/views/peca/index.blade.php
@@ -3,9 +3,27 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-9">
+        <div class="col-md-2">
             <h3><i class="fas fa-tools text-primary"></i> Peças </li> </h3> 
         </div>
+        <div class="col-md-7">
+            <form action="" method="POST" id="formPesquisarPeca">
+            @csrf
+                <div class="col-md-4">
+                    <input type="text" class="form-control" id="PesquisaPeca" required="true" placeholder="Pesquisar peça...">
+                    <select class="custom-select custom-select-sm">
+                        <option selected>Selecione um filtro</option>
+                        <option value="1">Título da peça</option>
+                        <option value="2">Código da peça</option>               
+                    </select>
+                </div>
+                <div class="col-md-3">
+                    <button type="submit" class="btn btn-primary">Pesquisar peça</button>
+                </div>
+            </form>   
+        </div>  
+        
+
         <div class="col-md-3">
             <a href="{{ route('peca.create') }}" class="btn btn-md bg-success text-light float-right"> <i class="fas fa-plus"></i>&nbsp;&nbsp;Nova peça</a>
         </div>      
