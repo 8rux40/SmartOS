305912329f1eaee1bd9806065ab35e84b992b654
JOAO PAULO ALVES <joaopauloiff2018@gmail.com>
2020-11-23 20:18:48 -0300

    Alteração no css, tela de exibição de peça apagada

--- app/Http/Controllers/PecaController.php
+++ app/Http/Controllers/PecaController.php
@@ -113,17 +113,6 @@ class PecaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
-        // Verifica se usuário tem permissões de acesso
-        
-
-        // Verifica se peca existe
-        $peca = Peca::find($id);
-        if (!isset($peca)) return abort(404);
-
-        return view('peca.edit', compact('peca'));
-    }
 
     /**
      * Update the specified resource in storage.
