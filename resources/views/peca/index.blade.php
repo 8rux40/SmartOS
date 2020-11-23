b978a85cc3dbd40068abcda431975b947c28ace1
Bruno Tardin <brunotardin20@gmail.com>
2020-11-23 19:40:00 -0300

    Reestruturado o visual da busca

--- resources/views/peca/index.blade.php
+++ resources/views/peca/index.blade.php
@@ -6,27 +6,21 @@
         <div class="col-md-2">
             <h3><i class="fas fa-tools text-primary"></i> Peças </li> </h3> 
         </div>
-        <div class="col-md-7">
-            <form action="" method="POST" id="formPesquisarPeca">
-            @csrf
-                <div class="col-md-4">
-                    <input type="text" class="form-control" id="PesquisaPeca" required="true" placeholder="Pesquisar peça...">
-                    <select class="custom-select custom-select-sm">
+        <div class="col-md-8">
+                <form class="form-inline" method="POST" id="formPesquisarPeca">
+                @csrf
+                    <select class="custom-select">
                         <option selected>Selecione um filtro</option>
                         <option value="1">Título da peça</option>
                         <option value="2">Código da peça</option>               
                     </select>
-                </div>
-                <div class="col-md-3">
-                <button type="submit" class="btn btn-primary">Pesquisar peça</button>
-                </div>
-            </form>   
-        </div>  
-        
-
-        <div class="col-md-3">
-            <a href="{{ route('peca.create') }}" class="btn btn-md bg-success text-light float-right"> <i class="fas fa-plus"></i>&nbsp;&nbsp;Nova peça</a>
-        </div>      
+                    <input type="text" class="form-control" id="PesquisaPeca" required="true" placeholder="Pesquisar peça...">
+                    <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
+                </form>   
+            </div>     
+            <div class="col-md-2">
+                <a href="{{ route('peca.create') }}" class="btn btn-md bg-success text-light float-right"> <i class="fas fa-plus"></i>&nbsp;&nbsp;Nova peça</a>
+        </div> 
     </div>
     <div class="mt-2"></div>
         <div class="row">
