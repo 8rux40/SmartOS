b482810dee33dc560d30862faa0d5aa6cc14bf30
Bruno Tardin <brunotardin20@gmail.com>
2020-12-11 09:26:52 -0300

    Atualizado menu das páginas de erro

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -65,82 +65,105 @@ h2 {
         </style>
     </head>
     <body>
-        <nav class="navbar navbar-expand-md navbar-dark bg-primary">
-            <div class="container">
-                <a class="navbar-brand" href="{{ route('home') }}">
-                    {{ config('app.name', 'SmartOS') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        @guest 
-                        @else 
-                            {{-- Cliente --}}
+      <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route('home') }}">
+                {{ config('app.name', 'SmartOS') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                    @guest 
+                    @else 
+                        {{-- Cliente --}}
+                        @can('gerenciar clientes')
                             <li class="nav-item ">
                                 <a class="nav-link text-light" href="{{ route('cliente.index') }}" role="button" >
-                                    Cliente
+                                    <i class="fas fa-users"></i>&nbsp;Cliente
                                 </a>
                             </li>
-                            {{-- Celular --}}
+                        @endcan
+                        {{-- Celular --}}
+                        @can('gerenciar celulares')
                             <li class="nav-item ">
                                 <a class="nav-link text-light" href="{{ route('celular.index') }}" role="button" >
-                                    Celular
+                                    <i class="fas fa-mobile-alt"></i>&nbsp;Celular
                                 </a>
                             </li>
-                            {{-- Orçamento --}}
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                    Orçamento
+                        @endcan
+                        {{-- Orçamento --}}
+                        @can('gerenciar orcamento')
+                            <li class="nav-item ">
+                                <a class="nav-link text-light" href="{{ route('orcamento.index') }}">
+                                    <i class="fas fa-coins"></i>&nbsp;Orçamento
                                 </a>
-                                <div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('orcamento.index') }}">
-                                        Gerenciar orçamento
-                                    </a>
-                                </div>
                             </li>
-                        @endguest
-                    </ul>
+                        @elsecan('informar orcamento')
+                            <li class="nav-item ">
+                                <a class="nav-link text-light" href="{{ route('orcamento.index') }}">
+                                    <i class="fas fa-coins"></i>&nbsp;Orçamento
+                                </a>
+                            </li>
+                        @endcan
+                        {{-- Ordem de servico --}}
+                        @can('consultar os')
+                            <li class="nav-item ">
+                                <a class="nav-link text-light" href="{{ route('ordemservico.index') }}" role="button" >
+                                    <i class="fas fa-clipboard-list"></i>&nbsp;Ordem de serviço
+                                </a>
+                            </li>
+                        @endcan
+                        {{-- Peca --}}
+                        @can('gerenciar pecas')
+                            <li class="nav-item ">
+                                <a class="nav-link text-light" href="{{ route('peca.index') }}">
+                                    <i class="fas fa-tools"></i>&nbsp;Peça
+                                </a>
+                            </li>
+                        @endcan
+                    @endguest
+                </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link text-light" href="{{ route('login') }}"><i class="fas fa-key"></i>   {{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            {{-- @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif --}}
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                   <i class="fas fa-user"></i> &nbsp; {{ Auth::user()->name }}
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="{{ route('login') }}"><i class="fas fa-key"></i>   {{ __('Login') }}</a>
+                            </li>
+                        @endif
+                        {{-- @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif --}}
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                               <i class="fas fa-user"></i> &nbsp; {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    <span><i class="fas fa-door-open"></i>&nbsp; Sair</span>
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        <span><i class="fas fa-door-open"></i>&nbsp; Sair</span>
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 
 
           <main>
